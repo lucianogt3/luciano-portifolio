ba73e4d3e4bd297aaf9d356c64f5ce8af68e80e9
lucianogt3 <enf.lucianolino@gmail.com>
2026-01-01 19:48:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  base: './'
-});
+  base: '/luciano-portifolio/', // ⬅️ IMPORTANTE: nome do seu repositório
+})
